d3babdd4bde17893d0dc6e5a0b93d9135b92e019
hanshika0013 <hanshika0072@gmail.com>
2025-07-25 13:26:08 +0530

    Fruitikha-website

--- js/index.js
+++ js/index.js
@@ -33,3 +33,11 @@ jQuery("#carousel").owlCarousel({
     }
   }
 });
+window.addEventListener("scroll", function () {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY > 50) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  });
