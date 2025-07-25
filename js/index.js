fa39cd25ad7e93faea59fc1cd60a733b6b802778
hanshika0013 <hanshika0072@gmail.com>
2025-07-25 16:16:57 +0530

    Fruitikha-website

--- js/index.js
+++ js/index.js
@@ -30,6 +30,15 @@ jQuery("#carousel").owlCarousel({
 
     1366: {
       items: 4
+    },
+    768:{
+      items: 3
+    },
+    576:{
+  items:2
+    },
+    360:{
+      items: 1
     }
   }
 });
